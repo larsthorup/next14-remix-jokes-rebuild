040c0e367bb04dd67f57800b895880005ff78d1e
Lars Thorup <lars@zealake.com>
2024-05-25 14:21:40 +0200

    Outline: server component as child to client component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import JokesList from './JokesList';
 import NavButton from './NavButton';
 import RandomJokeButton from './RandomJokeButton';
+import Outline from './Outline';
 
 export default function Sidebar() {
   return (
     <div className="flex min-w-max flex-col gap-y-5">
       <RandomJokeButton />
-      <p>Here are a few more jokes to check out:</p>
-      <JokesList />
+      <Outline text="Here are a few more jokes to check out:">
+        <JokesList />
+      </Outline>
       <NavButton href="/jokes/new">Add your own</NavButton>
     </div>
   );
